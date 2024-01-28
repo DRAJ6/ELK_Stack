67cf101668f4723b6b62f5bdaac75dda8b0a7209
DRAJ6 <144625117+DRAJ6@users.noreply.github.com>
2024-01-28 21:21:37 +0530

    Add files via upload

--- train_model.py
+++ train_model.py
@@ -2,6 +2,8 @@ import logging
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, confusion_matrix
+import numpy as np
 
 # Configure the logging module
 logging.basicConfig(filename='training.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -26,8 +28,30 @@ model.fit(X_train, y_train)
 logging.info("Model training completed.")
 
 # Evaluate the model
+predictions = model.predict(X_test)
 score = model.score(X_test, y_test)
 logging.info(f"Model accuracy on test data: {score:.2f}")
+
+# Additional metrics
+f1 = f1_score(y_test, predictions, average='weighted')  # Use 'weighted' for multiclass F1 score
+conf_matrix = confusion_matrix(y_test, predictions)
+
+tp = np.diag(conf_matrix)
+tn = np.sum(conf_matrix) - (np.sum(conf_matrix, axis=0) + np.sum(conf_matrix, axis=1) - tp)
+fp = np.sum(conf_matrix, axis=0) - tp
+fn = np.sum(conf_matrix, axis=1) - tp
+# Log additional metric
+fp_rate = fp / (fp + tn)
+fn_rate = fn / (fn + tp)
+
+# Log additional metrics
+logging.info(f"F1 Score: {f1:.2f}")
+logging.info(f"True Negative: {tn}")
+logging.info(f"False Positive: {fp}")
+logging.info(f"False Negative: {fn}")
+logging.info(f"True Positive: {tp}")
+logging.info(f"False Positive Rate: {fp_rate:}")
+logging.info(f"False Negative Rate: {fn_rate:}")
 
 # Log model parameters
 logging.info(f"Model coefficients: {model.coef_}")
